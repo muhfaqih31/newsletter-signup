7be759622a37a25b85727c448efe9754c263cb8b
muhfaqih31 <mfaqih6@yahoo.com>
2022-08-29 19:27:30 +0700

    Change API implementation to follow Mailchimp official documentation without HTTP request. Use mailchimp_marketing NPM instead.

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
-const https = require('https');
+
+const mailchimp = require('@mailchimp/mailchimp_marketing');
 require('dotenv').config()
 
 
@@ -11,55 +12,42 @@ const port = 3000;
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
+
+
 app.get('/', (req,res) => {
   res.sendFile(__dirname + "/signup.html");
 });
 
-app.post('/', (req,res) => {
-  const firstName = req.body.fname
-  const lastName = req.body.lname;
-  const email = req.body.email;
+app.post('/', (req, res) => {
+    mailchimp.setConfig({
+      apiKey: process.env.api_key,
+      server: process.env.server
+    });
 
-  var data = {
-    members: [
-      {
-        email_address: email,
-        status: "subscribed",
-        merge_fields: {
+    const firstName = req.body.fname
+    const lastName = req.body.lname;
+    const email = req.body.email;
+    const listId = process.env.list_id;
+
+    async function run() {
+      const response = await mailchimp.lists.addListMember(listId, {
+          email_address: email,
+          status: "subscribed",
+          merge_fields: {
           FNAME: firstName,
           LNAME: lastName
         }
+      });
+
+      res.sendFile(__dirname + '/success.html');
+
+      if (response.errors.length) {
+        throw new Error(response.errors);
       }
-    ]
-  }
-
-  var jsonData = JSON.stringify(data);
-
-  const url = process.env.url_list
-  const options = {
-    method: "POST",
-    auth: process.env.api_key
-  };
-
-  const request = https.request(url, options, (response) => {
-    if(response.statusCode === 200) {
-      res.sendFile(__dirname + "/success.html");
     }
 
-    else {
-      res.sendFile(__dirname + "/failure.html");
-    }
-
-    response.on("data", data => {
-      console.log(JSON.parse(data));
-    });
-  });
-
-  request.write(jsonData);
-  request.end();
-
-
-
+    run()
+    .catch(errors => res.sendFile(__dirname + '/failure.html'));
 });
 
 app.post('/failure', (req,res) => {
